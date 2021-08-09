3396a1875f0f804a5a82430efa79c8452d3ba4eb
Frederik Høngaard <mail@frederikhoengaard.com>
2021-08-09 16:15:37 +0200

    Update preprocessing.py

--- easy-learn/preprocessing.py
+++ easy-learn/preprocessing.py
@@ -1,7 +1,29 @@
+import numpy as np
+import torch
+
+from sklearn.model_selection import train_test_split
 
 
-def dimensionality_reduction(
-    method='PCA',
-    retained_variance=0.99
-    ):
+def dataset_split(
+        features,
+        labels,
+        test_size=0.2,
+        shuffle=True,
+        random_state=None,
+        stratify=False,
+        to_tensor=False
+    ) -> list:
+    X_train, X_test, y_train, y_test = train_test_split(features,labels,test_size=test_size,shuffle=shuffle,random_state=random_state,stratify=stratify)
+    if to_tensor:
+        return torch.FloatTensor(X_train), torch.FloatTensor(X_test), torch.LongTensor(y_train), torch.LongTensor(y_test)
+    else:
+        return X_train, X_test, y_train, y_test
+
+
+
+def main():
     pass
+
+
+if __name__ == '__main__':
+    main()
